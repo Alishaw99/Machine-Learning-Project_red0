34368184dbe9f99cac7a57e208a6aa4fad79c7c5
Alishaw99 <tariqham@gmail.com>
2024-10-29 13:01:05 -0700

    folder added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "visa_approval",
+    version= "0.0.0",
+    author="Tariq",
+    author_email="tariqham@gmail.com",
+    packages=find_packages()
+)
